6eff4a63121e3360b7a0591c7864b6629134b658
alotofhee <ekgmljeong@gmail.com>
2025-05-16 09:40:55 +0900

    feat: 참여신청 카드 작성자 생년월일 삭제, 참여인원 표시

--- src/components/posts/JoinProfileCard.tsx
+++ src/components/posts/JoinProfileCard.tsx
@@ -11,13 +11,13 @@ const JoinProfileCard = ({ post }: { post: Post }) => {
   return (
     <Card className="p-4 w-full rounded-xl text-sm bg-white gap-2">
       <div className="flex items-center gap-2">
-        <Avatar className="w-24 h-24">
+        <Avatar className="w-20 h-20">
           <AvatarImage src="/public/images/cat.jpg" />
           <AvatarFallback>{post.userName}</AvatarFallback>
         </Avatar>
         <div>
           <div className="pl-2 pt-2 text-lg font-semibold mb-2">{post.userName}</div>
-          <p className="text-sm text-gray-500">2001.01.08</p>
+          {/* <p className="text-sm text-gray-500">2001.01.08</p> */}
         </div>
       </div>
       <div className="flex items-center gap-2 text-sm text-gray-50 mt-2">
@@ -26,7 +26,9 @@ const JoinProfileCard = ({ post }: { post: Post }) => {
       </div>
       <div className="flex items-center gap-2 text-sm text-gray-50 mt-2">
         <Users className="w-4 h-4" />
-        <span>{post.location}</span>
+        <span>
+          {post.currentParticipants} / {post.maxParticipants}
+        </span>
       </div>
       <div className="flex items-center gap-2 text-sm text-gray-50 mt-2">
         <MapPin className="w-4 h-4" />
